91ad651dfad4e47ad5a352ddf08d73991dc2fbf2
Brad Bernard <bradbernard@me.com>
2017-03-15 11:14:02 -0700

    Added VC

---
+++ ZeroBalance/DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  ZeroBalance
+//
+//  Created by Brad Bernard on 3/14/17.
+//  Copyright © 2017 Brad Bernard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@end
