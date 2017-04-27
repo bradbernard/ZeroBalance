20606bc451d4976b02cf3fcbf7768986d4438185
Brad Bernard <bradbernard@me.com>
2017-04-27 14:17:58 -0700

    Starting charts on payment percentages

--- ZeroBalance/DetailViewController.m
+++ ZeroBalance/DetailViewController.m
@@ -8,6 +8,8 @@
 
 #import "DetailViewController.h"
 #import "TransactionMO+CoreDataClass.h"
+#import "PaymentMO+CoreDataClass.h"
+#import "PNChart.h"
 
 @interface DetailViewController ()
 
@@ -21,11 +23,52 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    if(self.transactionId != nil) {
-        TransactionMO* transaction = [self.managedObjectContext objectWithID:self.transactionId];
-        self.amountLabel.text = [NSNumberFormatter localizedStringFromNumber:[NSNumber numberWithDouble:transaction.total] numberStyle:NSNumberFormatterCurrencyStyle];
+    if(self.transactionId == nil) {
+        return;
     }
+    
+//        self.amountLabel.text = [NSNumberFormatter localizedStringFromNumber:[NSNumber numberWithDouble:transaction.total] numberStyle:NSNumberFormatterCurrencyStyle];
+    [self createChart];
 }
+
+- (UIColor *)generateColor {
+    CGFloat hue = ( arc4random() % 256 / 256.0 );  //  0.0 to 1.0
+    CGFloat saturation = ( arc4random() % 128 / 256.0 ) + 0.5;  //  0.5 to 1.0, away from white
+    CGFloat brightness = ( arc4random() % 128 / 256.0 ) + 0.5;  //  0.5 to 1.0, away from black
+    return [UIColor colorWithHue:hue saturation:saturation brightness:brightness alpha:1];
+}
+
+- (void)createChart {
+    
+    TransactionMO* transaction = [self.managedObjectContext objectWithID:self.transactionId];
+    PaymentMO *payment;
+    NSMutableArray *items = [[NSMutableArray alloc] init];
+    
+    for (payment in transaction.payments) {
+        if(payment.paid != 0)
+        [items addObject:[PNPieChartDataItem dataItemWithValue:payment.paid color:[self generateColor] description:payment.name]];
+    }
+    
+//    NSArray *items = @[[PNPieChartDataItem dataItemWithValue:10 color:PNRed],
+//                       [PNPieChartDataItem dataItemWithValue:20 color:PNBlue description:@"WWDC"],
+//                       [PNPieChartDataItem dataItemWithValue:40 color:PNGreen description:@"GOOL I/O"],
+//                       ];
+    
+    PNPieChart *pieChart = [[PNPieChart alloc] initWithFrame:CGRectMake(40.0, 155.0, 240.0, 240.0) items:items];
+    pieChart.descriptionTextColor = [UIColor whiteColor];
+    pieChart.descriptionTextFont  = [UIFont fontWithName:@"Avenir-Medium" size:14.0];
+    pieChart.showAbsoluteValues = false;
+    pieChart.showOnlyValues = true;
+    [pieChart strokeChart];
+    
+    pieChart.legendStyle = PNLegendItemStyleStacked;
+    UIView *legend = [pieChart getLegendWithMaxWidth:200];
+    [legend setFrame:CGRectMake(330, 350, legend.frame.size.width, legend.frame.size.height)];
+
+    [self.view addSubview:legend];
+    [self.view addSubview:pieChart];
+}
+
 
 - (IBAction)sendSMSTapped:(id)sender {
     NSLog(@"SMS tapped");
